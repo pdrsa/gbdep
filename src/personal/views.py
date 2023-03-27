d6e4894d73977ecbdd443643cf032bf3cdb867d6
pdrsa <ja1antoniolp@gmail.com>
2023-03-27 12:56:21 -0300

    First commit to this branch. I will do my Django practice here.

--- src/personal/views.py
+++ src/personal/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 def home_screen_view(request):
     print(request.headers)
 
-    return render(request, "index.html", {})
+    return render(request, "personal/home.html", {})
