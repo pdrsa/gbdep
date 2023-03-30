c8c7fd4e9751ba39612bdbfb8975debd3a34d457
bernardoamorim <bernardotamorim@gmail.com>
2023-03-29 21:19:39 -0300

    Remove front, add requirements and small style changes.

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from personal.views import home_screen_view
+from personal.views import {
+    home_screen_view,
+}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
